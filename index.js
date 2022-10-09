39055188c13c86f68b0c3bb54722b72272608a4a
vunt199802-coder <vunt199802@gmail.com>
2022-10-09 23:10:18 +0530

    initial deploy

---
+++ index.js
@@ -0,0 +1,49 @@
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+const SerpApi = require("google-search-results-nodejs");
+const search = new SerpApi.GoogleSearch(process.env.SERPAPI_KEY);
+
+bot.start((ctx) => ctx.reply("Welcome to Third Eye Image Finder"));
+bot.help((ctx) =>
+  ctx.reply("Type Name and The Bot will flood ur chat with relevant images")
+);
+// bot.on("sticker", (ctx) => ctx.reply("👍"));
+// bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.on("text", (ctx) => {
+  const params = {
+    device: "desktop",
+    engine: "google",
+    ijn: "0",
+    q: ctx.message.text,
+    google_domain: "google.com",
+    tbm: "isch",
+  };
+  console.log(ctx.message);
+  const callback = function (data) {
+    const image = data.images_results.map((item, index) => item.original);
+
+    console.log(image[0]);
+    // ctx.sendChatAction("upload_photo");
+
+    ctx.sendChatAction("upload_document");
+    // ctx.replyWithPhoto(image[0]);
+    var myStringArray = image;
+    var arrayLength = myStringArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+      console.log(myStringArray[i]);
+      ctx.replyWithPhoto(myStringArray[i]);
+      //Do something
+    }
+  };
+
+  // Show result as JSON
+  search.json(params, callback);
+  // send a message to the chat acknowledging receipt of their message
+});
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
